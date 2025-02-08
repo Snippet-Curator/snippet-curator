44c41be32cca42261761d90892509c348dbe571c
Kang <thisispiggy@gmail.com>
2025-02-08 13:39:06 -0500

    added basic layout

--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
+	import Sidebar from '$lib/components/Sidebar.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="drawer lg:drawer-open">
+	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content">
+		<div class="navbar bg-base-200 shadow-sm">
+			<label for="my-drawer-2" class="btn btn-square lg:hidden"> = </label>
+			<input type="text" placeholder="Search..." class="input w-full" />
+		</div>
+		{@render children()}
+	</div>
+	<div class="drawer-side border-base-300 border-r">
+		<label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+		<ul class="menu bg-base-200 text-base-content min-h-full w-64 p-4">
+			<!-- Sidebar content here -->
+			<li><a>Sidebar Item 1</a></li>
+			<li><a>Sidebar Item 2</a></li>
+		</ul>
+	</div>
+</div>
