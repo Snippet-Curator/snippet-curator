9f497782bc50bea5adce13a3a40e8b1bf202722e
Kang <thisispiggy@gmail.com>
2025-02-17 23:05:04 -0500

    fixed cards

---
+++ electron-app/db/pb_migrations/1739850809_updated_notes.js
@@ -0,0 +1,42 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3395098727")
+
+  // update field
+  collection.fields.addAt(13, new Field({
+    "hidden": false,
+    "id": "file1204091606",
+    "maxSelect": 99,
+    "maxSize": 1000,
+    "mimeTypes": [],
+    "name": "attachments",
+    "presentable": false,
+    "protected": false,
+    "required": false,
+    "system": false,
+    "thumbs": [],
+    "type": "file"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3395098727")
+
+  // update field
+  collection.fields.addAt(13, new Field({
+    "hidden": false,
+    "id": "file1204091606",
+    "maxSelect": 99,
+    "maxSize": 0,
+    "mimeTypes": [],
+    "name": "attachments",
+    "presentable": false,
+    "protected": false,
+    "required": false,
+    "system": false,
+    "thumbs": [],
+    "type": "file"
+  }))
+
+  return app.save(collection)
+})
--- svelte-app/src/lib/components/NoteList.svelte
+++ svelte-app/src/lib/components/NoteList.svelte
@@ -4,23 +4,23 @@
 </script>
 
 <div
-	class="min-w-0 columns-1 space-x-4 space-y-6 px-6 pb-10 pt-10 md:columns-2 md:px-10 md:pt-8 lg:columns-3 2xl:columns-4"
+	class="columns-1 gap-x-8 space-y-6 px-6 pb-10 pt-10 md:columns-2 md:px-10 md:pt-8 lg:columns-3 2xl:columns-4"
 >
 	{#if notes?.items.length > 0}
 		{#each notes.items as note}
 			<button
 				onclick={() => goto(`#/note/${note.id}`)}
-				class="card hover:bg-base-200/70 bg-base-100 card-border border transition-colors duration-300 hover:cursor-pointer"
+				class="card hover:bg-base-200/70 bg-base-100 card-border w-full border transition-colors duration-300 hover:cursor-pointer"
 			>
-				<figure>
+				<figure class="w-full">
 					<img src="http://127.0.0.1:8090/api/files/notes/{note.id}/{note.attachments[0]}" alt="" />
 				</figure>
-				<div class="card-body pb-2 pt-5">
+				<div class="card-body w-full pb-2 pt-5">
 					<div class="card-title text-left">
 						{note.title}
 					</div>
 					{#if note.attachments.length == 0}
-						{@html note.content.slice(0, 200)}
+						<div class="text-left">{@html note.content.slice(0, 200)}</div>
 					{/if}
 					<div>
 						{#each note.expand.tags as tag}
