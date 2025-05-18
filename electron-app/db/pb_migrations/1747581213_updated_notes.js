4d5e125d433967c847ec8a067ea85c10b58a715e
kangruixiang <kangruixiang@gmail.com>
2025-05-18 11:19:47 -0400

    max file size at 200

--- electron-app/db/pb_migrations/1747581213_updated_notes.js
+++ electron-app/db/pb_migrations/1747581213_updated_notes.js
@@ -1,46 +1,51 @@
 /// <reference path="../pb_data/types.d.ts" />
-migrate((app) => {
-  const collection = app.findCollectionByNameOrId("pbc_3395098727")
+migrate(
+  (app) => {
+    const collection = app.findCollectionByNameOrId('pbc_3395098727')
 
-  // update field
-  collection.fields.addAt(18, new Field({
-    "hidden": false,
-    "id": "file1204091606",
-    "maxSelect": 500,
-    "maxSize": 1000000000,
-    "mimeTypes": [],
-    "name": "attachments",
-    "presentable": false,
-    "protected": false,
-    "required": false,
-    "system": false,
-    "thumbs": [
-      "500x0"
-    ],
-    "type": "file"
-  }))
+    // update field
+    collection.fields.addAt(
+      18,
+      new Field({
+        hidden: false,
+        id: 'file1204091606',
+        maxSelect: 200,
+        maxSize: 1000000000,
+        mimeTypes: [],
+        name: 'attachments',
+        presentable: false,
+        protected: false,
+        required: false,
+        system: false,
+        thumbs: ['500x0'],
+        type: 'file'
+      })
+    )
 
-  return app.save(collection)
-}, (app) => {
-  const collection = app.findCollectionByNameOrId("pbc_3395098727")
+    return app.save(collection)
+  },
+  (app) => {
+    const collection = app.findCollectionByNameOrId('pbc_3395098727')
 
-  // update field
-  collection.fields.addAt(18, new Field({
-    "hidden": false,
-    "id": "file1204091606",
-    "maxSelect": 99,
-    "maxSize": 1000000000,
-    "mimeTypes": [],
-    "name": "attachments",
-    "presentable": false,
-    "protected": false,
-    "required": false,
-    "system": false,
-    "thumbs": [
-      "500x0"
-    ],
-    "type": "file"
-  }))
+    // update field
+    collection.fields.addAt(
+      18,
+      new Field({
+        hidden: false,
+        id: 'file1204091606',
+        maxSelect: 99,
+        maxSize: 1000000000,
+        mimeTypes: [],
+        name: 'attachments',
+        presentable: false,
+        protected: false,
+        required: false,
+        system: false,
+        thumbs: ['500x0'],
+        type: 'file'
+      })
+    )
 
-  return app.save(collection)
-})
+    return app.save(collection)
+  }
+)
