fe38c76f876e8af437f172fe240c2df97cd799ad
kangruixiang <kangruixiang@gmail.com>
2025-06-02 15:56:27 -0400

    added index for score

---
+++ electron-app/db/pb_migrations/1748894156_updated_notes.js
@@ -0,0 +1,29 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3395098727")
+
+  // update collection data
+  unmarshal({
+    "indexes": [
+      "CREATE UNIQUE INDEX `idx_yirkdTS2kY` ON `notes` (\n  `title`,\n  `added`\n)",
+      "CREATE INDEX `idx_notes_notebook_created` ON `notes` (\n  `status`,\n  `created` DESC\n)",
+      "CREATE INDEX `idx_4NuxBc5Iu6` ON `notes` (`created` DESC)",
+      "CREATE INDEX `idx_Ul1ojw2TsH` ON `notes` (`score` DESC)"
+    ]
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3395098727")
+
+  // update collection data
+  unmarshal({
+    "indexes": [
+      "CREATE UNIQUE INDEX `idx_yirkdTS2kY` ON `notes` (\n  `title`,\n  `added`\n)",
+      "CREATE INDEX `idx_notes_notebook_created` ON `notes` (\n  `status`,\n  `created` DESC\n)",
+      "CREATE INDEX `idx_4NuxBc5Iu6` ON `notes` (`created` DESC)"
+    ]
+  }, collection)
+
+  return app.save(collection)
+})
