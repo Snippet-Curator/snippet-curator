a6eb2bc89d9e855cb33d03225d5761deb2322130
kangruixiang <kangruixiang@gmail.com>
2025-06-04 21:09:59 -0400

    added randomness to new notes

--- electron-app/db/pb_hooks/utils.js
+++ electron-app/db/pb_hooks/utils.js
@@ -10,7 +10,7 @@ function normalizeWeight(val, min, max) {
 function recencyScore(lastOpened) {
   const openedDate = new Date(lastOpened)
   if (isNaN(openedDate.getTime())) {
-    return 1
+    return 0.3 + Math.random() * 0.7
   }
   const daysAgo = (Date.now() - openedDate.getTime()) / (1000 * 60 * 60 * 24)
 
