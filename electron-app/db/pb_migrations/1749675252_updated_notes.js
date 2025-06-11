7b9ca16bb0c7292930802b06f197065e6d5b88aa
kangruixiang <kangruixiang@gmail.com>
2025-06-11 17:37:47 -0400

    added thumbnail 200

---
+++ electron-app/db/pb_migrations/1749675252_updated_notes.js
@@ -0,0 +1,48 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3395098727")
+
+  // update field
+  collection.fields.addAt(16, new Field({
+    "hidden": false,
+    "id": "file1204091606",
+    "maxSelect": 200,
+    "maxSize": 1000000000,
+    "mimeTypes": [],
+    "name": "attachments",
+    "presentable": false,
+    "protected": false,
+    "required": false,
+    "system": false,
+    "thumbs": [
+      "500x0",
+      "200x0"
+    ],
+    "type": "file"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3395098727")
+
+  // update field
+  collection.fields.addAt(16, new Field({
+    "hidden": false,
+    "id": "file1204091606",
+    "maxSelect": 200,
+    "maxSize": 1000000000,
+    "mimeTypes": [],
+    "name": "attachments",
+    "presentable": false,
+    "protected": false,
+    "required": false,
+    "system": false,
+    "thumbs": [
+      "500x0",
+      "100x0"
+    ],
+    "type": "file"
+  }))
+
+  return app.save(collection)
+})
