ccf8fc1a000f0d988af3fa5e6f77df9705691e22
kangruixiang <kangruixiang@gmail.com>
2025-06-15 16:51:16 -0400

    cleaned up layout

--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -3,14 +3,7 @@
 	import { page } from '$app/state';
 	import { onMount } from 'svelte';
 
-	import {
-		Import,
-		Notebook as NotebookIcon,
-		Settings,
-		Tags,
-		WalletCards,
-		Archive
-	} from 'lucide-svelte';
+	import { Import, Notebook as NotebookIcon, Settings, Tags, WalletCards } from 'lucide-svelte';
 	import { ScrollArea } from '$lib/components/ui/scroll-area/index';
 	import * as Resizable from '$lib/components/ui/resizable/index.js';
 
@@ -166,78 +159,5 @@
 		{@render children()}
 	</Resizable.Pane>
 </Resizable.PaneGroup>
-
-<!-- <div class="drawer lg:drawer-open font-display">
-	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-content">
-		<div>
-			{@render children()}
-		</div>
-	</div>
-	<div class="drawer-side border-base-300 border-r">
-		<label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
-
-		<ul class="menu bg-base-200 min-h-screen w-64 space-y-2 p-4">
-			<div class="m-golden-md flex h-8 items-center gap-x-1">
-				<Icon /> <span class="text-xl font-semibold tracking-widest">ecollect</span>
-			</div>
-
-			<li>
-				<a class={page.url.hash == '#/discover' ? 'menu-active' : ''} href="#/discover">Discover</a>
-			</li>
-			<li>
-				<a class={page.url.hash == '#/' || page.url.hash == '' ? 'menu-active' : ''} href="#/"
-					>Search</a
-				>
-			</li>
-			{#await defaultNotebooks then}
-				<li>
-					<a
-						class="{page.url.hash == `#/notebook/${defaultNotebooksState.inboxID}`
-							? 'menu-active'
-							: ''} flex w-full justify-between"
-						href="#/notebook/{defaultNotebooksState.inboxID}"
-						><span>Inbox</span> {defaultNotebooksState.inboxCount}</a
-					>
-				</li>
-			{/await}
-
-			<div class="divider my-0 py-0"></div>
-
-			<ScrollArea class="h-10 grow">
-				<span class="menu-title flex max-h-60 items-center gap-2 overflow-y-auto"
-					><NotebookIcon size={18} />Notebooks</span
-				>
-
-				<NotebookList notebooks={notebookState.notebooks} />
-
-				<span class="menu-title flex items-center gap-2"><Tags size={18} /> Tags</span>
-
-				<TagList tags={tagState.tags} />
-			</ScrollArea>
-
-			{#snippet renderBottomPages(name: string, url: string, icon: any)}
-				{@const Icon = icon}
-				<li>
-					<a class={page.url.hash == url ? 'menu-active' : ''} href={url}>
-						<Icon size={18} />
-						{name}</a
-					>
-				</li>
-			{/snippet}
-
-			{#await defaultNotebooks then}
-				{@render renderBottomPages(
-					'Archive',
-					`#/notebook/${defaultNotebooksState.archiveID}`,
-					Archive
-				)}
-			{/await}
-			{#each bottomPages as page}
-				{@render renderBottomPages(page.name, page.url, page.icon)}
-			{/each}
-		</ul>
-	</div>
-</div> -->
 
 <Dock />
