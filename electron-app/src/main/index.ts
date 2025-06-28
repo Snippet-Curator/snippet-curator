29013e270119a3e2177e092560b434984e467f0c
kangruixiang <kangruixiang@gmail.com>
2025-06-28 10:47:51 -0400

    fixed notification appID

--- electron-app/src/main/index.ts
+++ electron-app/src/main/index.ts
@@ -224,7 +224,7 @@ app.whenReady().then(async () => {
     }
 
     // Set app user model id for windows
-    electronApp.setAppUserModelId('com.electron')
+    electronApp.setAppUserModelId('com.krxiang.curator')
 
     // Default open or close DevTools by F12 in development
     // and ignore CommandOrControl + R in production.
