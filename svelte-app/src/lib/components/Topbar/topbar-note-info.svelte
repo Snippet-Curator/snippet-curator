c17a6c1da03193879575e95c3103fae4558d85a8
kangruixiang <kangruixiang@gmail.com>
2025-07-05 22:37:58 -0400

    added popover

--- svelte-app/src/lib/components/Topbar/topbar-note-info.svelte
+++ svelte-app/src/lib/components/Topbar/topbar-note-info.svelte
@@ -2,6 +2,7 @@
 	import type { Note } from '$lib/types';
 	import dayjs from 'dayjs';
 	import { Info } from 'lucide-svelte';
+	import * as Popover from '$lib/components/ui/popover/index';
 
 	type Props = {
 		note: Note;
@@ -29,19 +30,10 @@
 	}
 </script>
 
-<div class="dropdown dropdown-end">
-	<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
-	<div class="tooltip tooltip-bottom z-30" data-tip="Info">
-		<div tabindex="0" class="btn btn-ghost"><Info size={18} /></div>
-	</div>
-	<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
-	<div
-		tabindex="0"
-		class="dropdown-content border-base-content/20 bg-base-100 rounded-box p-golden-md min-w-80 border shadow-sm"
-	>
-		<div class="gap-golden-md grid grid-cols-3 p-2">
-			<!-- <div>status</div>
-			<div class="col-span-2">{note.status}</div> -->
+<Popover.Root>
+	<Popover.Trigger><div class="btn btn-ghost"><Info size={18} /></div></Popover.Trigger>
+	<Popover.Content>
+		<div class="gap-golden-md grid grid-cols-3">
 			<div>Last Opened</div>
 			<div class="col-span-2">{lastOpened}</div>
 			<div>Created</div>
@@ -66,5 +58,43 @@
 				<div>{size} MB</div>
 			{/if}
 		</div>
-	</div>
-</div>
+	</Popover.Content>
+</Popover.Root>
+
+<!-- <div class="dropdown dropdown-end"> -->
+<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
+<!-- <div class="tooltip tooltip-bottom z-30" data-tip="Info">
+		<div tabindex="0" class="btn btn-ghost"><Info size={18} /></div>
+	</div> -->
+<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
+<!-- <div
+		tabindex="0"
+		class="dropdown-content border-base-content/20 bg-base-100 rounded-box p-golden-md min-w-80 border shadow-sm"
+	>
+		<div class="gap-golden-md grid grid-cols-3 p-2">
+			<div>Last Opened</div>
+			<div class="col-span-2">{lastOpened}</div>
+			<div>Created</div>
+			<div class="col-span-2">{created}</div>
+			<div>Modified</div>
+			<div class="col-span-2">{modified}</div>
+
+			{#each note.sources as source}
+				<div>Source</div>
+				<div class="col-span-2">{source.source}</div>
+
+				<div>URL</div>
+				<div class="col-span-2 break-all">
+					<a class="link line-clamp-2" href={source.source_url} onclick={openURL}
+						>{source.source_url}</a
+					>
+				</div>
+			{/each}
+
+			{#if size}
+				<div class="text-nowrap">Size</div>
+				<div>{size} MB</div>
+			{/if}
+		</div>
+	</div> -->
+<!-- </div> -->
---
+++ svelte-app/src/lib/components/ui/popover/popover-content.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { cn } from '$lib/utils.js';
+	import { Popover as PopoverPrimitive } from 'bits-ui';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		sideOffset = 4,
+		align = 'center',
+		portalProps,
+		...restProps
+	}: PopoverPrimitive.ContentProps & {
+		portalProps?: PopoverPrimitive.PortalProps;
+	} = $props();
+</script>
+
+<PopoverPrimitive.Portal {...portalProps}>
+	<PopoverPrimitive.Content
+		bind:ref
+		data-slot="popover-content"
+		{sideOffset}
+		{align}
+		class={cn(
+			'bg-base-100 border-base-content/20 text-base-content data-[state=closed]:motion-opacity-out-0 data-[state=open]:motion-opacity-in-0 data-[state=closed]:motion-scale-out-95 motion-duration-75 data-[state=open]:motion-scale-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-(--bits-popover-content-transform-origin) outline-hidden font-display z-50 w-72 rounded-md border p-4 text-sm shadow-md',
+			className
+		)}
+		{...restProps}
+	/>
+</PopoverPrimitive.Portal>
